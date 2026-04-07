3b0308f7c419440cc09f39664b9c82c4e6add91e
Krushovice <kickstar69@yandex.ru>
2026-04-07 17:10:47 +1000

    import models

---
+++ src/core/database/models/__init__.py
@@ -0,0 +1,18 @@
+__all__ = (
+    "Base",
+    "Mission",
+    "MissionCharacter",
+    "Character", "User",
+    "MissionCharacter",
+    "Character",
+    "Event",
+)
+
+from .user_mission import UserMission
+from .user import User
+from .user_resource import UserResource
+from .mission import Mission
+from .mission_character import MissionCharacter
+from .character import Character
+from .event import Event
+from .base import Base
